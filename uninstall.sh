d7d63a2d09fd2f5362613a1c642ef183b370687d
Roberto Jiménez <powermx@gmail.com>
2023-07-28 18:58:36 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,13 +6,13 @@ systemctl stop zivpn.services 1> /dev/null 2> /dev/null
 killall zivpn 1> /dev/null 2> /dev/null
 rm -rf /etc/zivpn 1> /dev/null 2> /dev/null
 rm /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
-ex=`/usr/local/bin/zivpn` 1> /dev/null 2> /dev/null
 if pgrep "zivpn" >/dev/null; then
   echo -e "Server Running"
 else
   echo -e "Server Stopped"
 fi
-if [ -e "$ex" ]; then
+file="/usr/local/bin/zivpn" 1> /dev/null 2> /dev/null
+if [ -e "$file" ]; then
   echo -e "Files still remaining, try again"
 else
   echo -e "Successfully Removed"
