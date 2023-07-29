cfedd72c211960a9a316a14a45b2bfb13b5d1dd7
Roberto Jiménez <powermx@gmail.com>
2023-07-28 19:09:16 -0600

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -19,7 +19,7 @@ if [ -e "$file" ] 1> /dev/null 2> /dev/null; then
 else
   echo -e "Successfully Removed"
 fi
-echo "Cleaning Cache & Swap Usage"
+echo "Cleaning Cache & Swap"
 echo 3 > /proc/sys/vm/drop_caches
 sysctl -w vm.drop_caches=3
 swapoff -a && swapon -a
